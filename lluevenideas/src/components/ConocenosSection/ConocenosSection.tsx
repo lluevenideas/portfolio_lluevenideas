f9f269ddea46bb829b39b44078c1c8db2b24fa04
tomas.avila <tomysykess@gmail.com>
2024-11-02 19:33:35 -0300

    update demo conocenos section

--- lluevenideas/src/components/ConocenosSection/ConocenosSection.tsx
+++ lluevenideas/src/components/ConocenosSection/ConocenosSection.tsx
@@ -1,5 +1,41 @@
 const ConocenosSection = () => {
-  return <div>ConocenosSection</div>;
+  return (
+    <div
+      className="flex flex-col bg-white text-black w-full min-h-screen items-center px-8 py-16"
+      style={{
+        backgroundImage: `url('/conocenos.jpg')`,
+        backgroundPosition: "bottom left",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "auto",
+      }}
+    >
+      <div className="w-[90%] h-full flex flex-col items-start z-10">
+        <h1 className="text-7xl font-extrabold mb-4">Conócenos</h1>
+        <div className="w-full h-[2px] bg-gradient-to-r from-white to-black mb-8"></div>
+
+        <div className="text-4xl leading-relaxed mb-8 w-full sm:w-7/12">
+          <p className="mb-4">
+            Somos un equipo de desarrolladores y diseñadores web, dinámicos e
+            innovadores.
+          </p>
+          <p>
+            Nos dedicamos a ayudar a las empresas a prosperar en el mundo
+            digital. Con nuestra pasión por la creatividad y la tecnología,
+            impulsamos a nuestros clientes a alcanzar sus objetivos a través de
+            soluciones de vanguardia y un servicio personalizado.
+          </p>
+        </div>
+
+        <div className="w-full h-[2px] bg-gradient-to-r from-black to-white mb-8"></div>
+
+        <div className="flex w-full justify-end">
+          <p className="text-blue-400 text-xl font-semibold mt-4 text-right w-1/4">
+            Diseñamos, desarrollamos y desplegamos sitios y aplicaciones web.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ConocenosSection;
