83f4292faef162051600b99fff1b7a92c9056ac4
SamantaDriuzzi <driuzzi.samanta.mayra@gmail.com>
2024-11-04 12:17:22 -0300

    Dejo responsivo el texto e img

--- lluevenideas/src/components/HeroSection/HeroSection.tsx
+++ lluevenideas/src/components/HeroSection/HeroSection.tsx
@@ -1,15 +1,24 @@
 import Image from "next/image";
 import Gota from "../../../public/hero/gota-agua.svg";
 
-const HeroSection = () => {
+const HeroSection: React.FC = () => {
   return (
-    <div className="w-full h-min-screen bg-[--primary-color]">
-      <div className="logo-font ">
-        LLev <span className="logo-font-ven">ven</span> ideas
-        <Image src={Gota} alt={"Imágen de una gota de agua"} />
+    <div className="w-full min-h-screen bg-[--primary-color]
+     flex flex-col items-center justify-center p-4 text-black ">
+      <div className="text-center flex items-end">
+        <h1 className="text-4xl sm:text-6xl md:text-8xl lg:text-[170px] xl:text-[170px]
+         logo-font ">
+          LLue<span className="logo-font-ven">ven</span>ideas
+        </h1>
+        <div className="sm:my-0 sm:mt-5">
+          <div className="relative w-2 h-4 sm:w-8 sm:h-10 md:w-12 md:h-14 
+          lg:w-16 lg:h-20 xl:w-20 xl:h-24 sm:mb-2.5 md:mb-2">
+            <Image src={Gota} alt="Imagen de una gota de agua" layout="fill" objectFit="contain" />
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default HeroSection;
