807edec3dd4efcbb7bc829013b2dcd7243c77cc4
SamantaDriuzzi <driuzzi.samanta.mayra@gmail.com>
2024-11-04 11:24:12 -0300

    Agrego las fuentes al proyecto en general

--- lluevenideas/src/app/page.tsx
+++ lluevenideas/src/app/page.tsx
@@ -1,9 +1,11 @@
 import AboutUsSection from "@/components/AboutUsSection/AboutUsSection";
 import ConocenosSection from "@/components/ConocenosSection/ConocenosSection";
+import HeroSection from "@/components/HeroSection/HeroSection";
 import ProjectsSection from "@/components/ProjectsSection/ProjectsSection";
 export default function Home() {
   return (
     <div>
+      <HeroSection/>
       <AboutUsSection></AboutUsSection>
       <ProjectsSection></ProjectsSection>
       <ConocenosSection></ConocenosSection>
--- lluevenideas/src/components/HeroSection/HeroSection.tsx
+++ lluevenideas/src/components/HeroSection/HeroSection.tsx
@@ -1,5 +1,15 @@
+import Image from "next/image";
+import Gota from "../../../public/hero/gota-agua.svg";
+
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div className="w-full h-min-screen bg-[--primary-color]">
+      <div className="logo-font ">
+        LLev <span className="logo-font-ven">ven</span> ideas
+        <Image src={Gota} alt={"Imágen de una gota de agua"} />
+      </div>
+    </div>
+  )
 };
 
 export default HeroSection;
