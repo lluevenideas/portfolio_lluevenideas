c9a17ab80afec2df4cd512e2e674469e257cade9
Evaristo <114101006+EvaristoAguirre@users.noreply.github.com>
2024-11-05 20:20:35 -0300

    ADD/contenido en seccion contacto

--- lluevenideas/src/components/ContactSection/ContactSection.tsx
+++ lluevenideas/src/components/ContactSection/ContactSection.tsx
@@ -7,7 +7,23 @@ const ContactSection = () => {
         backgroundRepeat: "no-repeat",
         backgroundSize: "auto",
       }}>
-
+        <div className="w-[90%] h-full flex flex-col items-start z-10 ">
+          <h1 className="text-7xl text-start font-extrabold mb-4">¿Cómo podemos ayudarte?</h1>
+          <div className="w-full h-[2px] bg-gradient-to-r from-black to-white mb-8"></div>
+          <div className="flex w-full h-full justify-center items-center">
+            <div className="relative w-72 h-96 overflow-hidden flex items-start justify-center z-0">
+              <p className="absolute font-extrabold text-3xl text-left w-60 h-40">Estoy interesado en:</p>
+            </div>
+            <div className="flex flex-col relative w-72 h-96 overflow-hidden flex items-center justify-center z-0">
+              <form >
+                <input type="text" placeholder="Nombre"/>
+                <input type="text" placeholder="Email"/>
+                <textarea placeholder="Mensaje"/>
+                <button>Enviar</button>
+              </form>
+            </div>
+          </div>
+        </div>
 
 
 
