5fd7ae1c9615dddeead9daaf0308a4fd55338e94
Evaristo <114101006+EvaristoAguirre@users.noreply.github.com>
2024-11-05 16:34:48 -0300

    ADD/rama_Evaristo y seccion de contacto

--- lluevenideas/src/app/page.tsx
+++ lluevenideas/src/app/page.tsx
@@ -1,12 +1,14 @@
 import AboutUsSection from "@/components/AboutUsSection/AboutUsSection";
 import ConocenosSection from "@/components/ConocenosSection/ConocenosSection";
 import ProjectsSection from "@/components/ProjectsSection/ProjectsSection";
+import ContactSection from "@/components/ContactSection/ContactSection";
 export default function Home() {
   return (
     <div>
       <AboutUsSection></AboutUsSection>
       <ProjectsSection></ProjectsSection>
       <ConocenosSection></ConocenosSection>
+      <ContactSection></ContactSection>
     </div>
   );
 }
--- lluevenideas/src/components/ContactSection/ContactSection.tsx
+++ lluevenideas/src/components/ContactSection/ContactSection.tsx
@@ -1,5 +1,19 @@
 const ContactSection = () => {
-  return <div>ContactSection</div>;
+  return <div
+  className="flex flex-col bg-white text-black w-full min-h-screen items-center px-8 py-16"
+      style={{
+        backgroundImage: `url('/contact.jpg')`,
+        backgroundPosition: "bottom left",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "auto",
+      }}>
+
+
+
+
+
+
+  </div>;
 };
 
 export default ContactSection;
