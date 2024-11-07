682e31c9968aaaab55d688f34f5e400ae9ca92fb
SamantaDriuzzi <driuzzi.samanta.mayra@gmail.com>
2024-11-07 15:50:15 -0300

    Seccion hero lista

--- lluevenideas/src/app/page.tsx
+++ lluevenideas/src/app/page.tsx
@@ -1,14 +1,11 @@
 import AboutUsSection from "@/components/AboutUsSection/AboutUsSection";
 import ConocenosSection from "@/components/ConocenosSection/ConocenosSection";
-import HeroPrueba from "@/components/HeroSection/hero-prueba";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import ProjectsSection from "@/components/ProjectsSection/ProjectsSection";
 export default function Home() {
   return (
     <div>
-      {/* <HeroSection/> */}
-      <HeroPrueba/>
-     
+      <HeroSection/>
       <AboutUsSection></AboutUsSection>
       <ProjectsSection></ProjectsSection>
       <ConocenosSection></ConocenosSection>
--- lluevenideas/src/components/HeroSection/HeroSection.tsx
+++ lluevenideas/src/components/HeroSection/HeroSection.tsx
@@ -1,32 +1,47 @@
-// HeroSection.tsx
 'use client';
-import { useState } from "react";
-import Image from "next/image";
-import Gota from "../../../public/hero/gota-agua.svg";
+import { useEffect, useRef } from "react";
+import lottie from "lottie-web";
 
-const HeroSection: React.FC = () => {
-  const [showAnimation, setShowAnimation] = useState<boolean>(false);
+const HeroPrueba = () => {
+  const lluevenIdeas = require("../../../public/hero/llueven_ideas.json")
 
-  const handleClickImage = () => {
-    setShowAnimation((prev) => !prev);
-  };
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let animationInstance: any;
+
+    if (containerRef.current) {
+      animationInstance = lottie.loadAnimation({
+        container: containerRef.current,
+        animationData: lluevenIdeas,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+      });
+
+
+      animationInstance.setSpeed(0.5);
+    }
+
+    return () => {
+      if (animationInstance) {
+        animationInstance.destroy();
+      }
+    };
+  }, [lluevenIdeas]);
 
   return (
-    <div className="w-full min-h-screen bg-[--primary-color] flex flex-col 
-    items-center justify-center p-4 text-black relative">
-      <div className="text-center flex items-end">
-        <h1 className="text-shadow text-4xl sm:text-6xl md:text-8xl 
-        lg:text-[170px] xl:text-[170px] logo-font">
-          LLue<span className="logo-font-ven">ven</span>ideas
-        </h1>
-        <div className="sm:my-0 sm:mt-5 cursor-pointer relative w-2 h-4 sm:w-8 
-        sm:h-10 md:w-12 md:h-14 lg:w-16 lg:h-20 xl:w-20 xl:h-24 sm:mb-2.5 md:mb-2"
-          onClick={handleClickImage}>
-          <Image src={Gota} alt="Imagen de una gota de agua" fill className="gota-animation" />
-        </div>
+    <div className="boxes-container w-full min-h-screen bg-black flex flex-col 
+    items-center justify-center p-4 text-black relative"
+      style={{ position: "relative", height: "100vh", overflow: "hidden" }}>
+      <div className="text-center flex items-end relative">
+        <div
+          ref={containerRef}
+        />
       </div>
+
     </div>
   );
 };
 
-export default HeroSection;
+export default HeroPrueba;
