aea5d5bde9d73bcc76511c8a5363cd2aaed3294f
SamantaDriuzzi <driuzzi.samanta.mayra@gmail.com>
2024-11-07 13:15:08 -0300

    ff

--- lluevenideas/src/components/HeroSection/hero-prueba.tsx
+++ lluevenideas/src/components/HeroSection/hero-prueba.tsx
@@ -5,57 +5,51 @@ import { useEffect, useRef } from "react";
 import lottie from "lottie-web";
 
 const HeroPrueba = () => {
-  const waterAnimation = require("../../../public/hero/animation-img/AnimationI.json");
+  const lluevenIdeas = require("../../../public/hero/animation-img/lluevenIdeas_bg_black.json")
 
   const containerRef = useRef<HTMLDivElement>(null);
+  const containerRefCloud =  useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    let animationInstance: lottie.AnimationItem | undefined;
+
     if (containerRef.current) {
-      lottie.loadAnimation({
+      animationInstance = lottie.loadAnimation({
         container: containerRef.current,
-        animationData: waterAnimation,
+        animationData: lluevenIdeas,
         renderer: "svg",
         loop: true,
         autoplay: true,
       });
+      
+      // Ajustar la velocidad de la animación
+      animationInstance.setSpeed(0.5); // Cambia 0.5 al valor que desees para ajustar la velocidad
     }
 
     return () => {
-      if (containerRef.current) {
-        lottie.destroy();
+      if (animationInstance) {
+        animationInstance.destroy();
       }
     };
-  }, [waterAnimation]);
+  }, [lluevenIdeas]);
 
   return (
-    <div className="boxes-container w-full min-h-screen bg-[--primary-color] flex flex-col 
+    <div className="boxes-container w-full min-h-screen bg-black flex flex-col 
     items-center justify-center p-4 text-black relative"
       style={{ position: "relative", height: "100vh", overflow: "hidden" }}>
-
+        <div ref={containerRefCloud}></div>
       <div className="text-center flex items-end relative">
-        <h1 className="text-shadow text-4xl sm:text-6xl md:text-8xl 
-        lg:text-[160px] xl:text-[160px] logo-font">
-          LLue<span className="logo-font-ven">ven</span>
-          {/* Animación Lottie directamente dentro de HeroPrueba */}
+     
           <div
             ref={containerRef}
-            style={{
-              width: "80px",  // Ajusta el tamaño según lo necesites
-              height: "100px", // Ajusta el tamaño según lo necesites
-              position: "absolute",
-              bottom: "20%",  // Alinea en la parte inferior de la palabra
-              left: "45%",
-              marginLeft: "90px",
-              transform: "translateX(-50%)",  // Centra la animación
-            }}
+            className=""
           />
-          <span className="text-transparent text-shadow-none ml-1 mr-1">i</span>deas
-        </h1>
+         
 
-        <div className="sm:my-0 sm:mt-5 cursor-pointer relative w-2 h-4 sm:w-8 
+        {/* <div className="sm:my-0 sm:mt-5 cursor-pointer relative w-2 h-4 sm:w-8 
         sm:h-10 md:w-12 md:h-14 lg:w-16 lg:h-20 xl:w-20 xl:h-24 sm:mb-2.5 md:mb-2">
           <Image src={Gota} alt="Imagen de una gota de agua" fill className="gota-animation" />
-        </div>
+        </div> */}
       </div>
 
     </div>
