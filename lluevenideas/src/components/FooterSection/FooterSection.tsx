56f6549bd17b178d5a7f9780174b90e8e42eb902
tomas.avila <tomysykess@gmail.com>
2024-11-09 13:51:18 -0300

    footer responsive, svg logo

--- lluevenideas/src/components/FooterSection/FooterSection.tsx
+++ lluevenideas/src/components/FooterSection/FooterSection.tsx
@@ -2,143 +2,48 @@ import Link from "next/link";
 
 const FooterSection = () => {
   return (
-    <div
-      className="footer"
-      style={{
-        height: "12rem",
-        backgroundColor: "#000000",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <div
-        className="contenedor"
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
-          width: "90%",
-          height: "100%",
-        }}
-      >
-        <div className="marca">
+    <div className="h-48 bg-black flex justify-center items-center">
+      <div className="flex flex-col lg:flex-row justify-around items-center w-11/12 h-full">
+        <div className="flex flex-col items-center lg:items-start text-center lg:text-left mb-4 lg:mb-0">
           <div
-            className="logo"
-            style={{
-              backgroundImage: `url('/lluevenideas..png')`,
-              backgroundRepeat: "no-repeat",
-              width: "16.5rem",
-              height: "2.5rem",
-            }}
+            className="bg-no-repeat w-64 h-10"
+            style={{ backgroundImage: "url('/lluevenideas.svg')" }}
           ></div>
-          <p
-            style={{
-              color: "#ffffff",
-            }}
-          >
-            Diseñadores y desarrolladores web.
+          <p className="text-white mt-2">Diseñadores y desarrolladores web.</p>
+        </div>
+
+        <div className="flex flex-col justify-end items-center h-3/4 my-4 lg:my-0">
+          <div className="w-full lg:w-[35rem] h-px bg-white"></div>
+          <p className="text-white mt-4">
+            (C) 2024 - LluevenIdeas Tecnologías - Todos los derechos reservados
           </p>
         </div>
 
-        <div
-          className="copyright"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-end",
-            alignItems: "center",
-            height: "70%",
-          }}
-        >
-          <div
-            className="separador"
-            style={{
-              width: "35rem",
-              height: "1px",
-              backgroundColor: "#FFFFFF",
-            }}
-          ></div>
-          <div className="derechos">
-            <p
-              style={{
-                color: "#FFFFFF",
-                marginTop: "1.2rem",
-              }}
-            >
-              (C) 2024 - LluevenIdeas Tecnologías - Todos los derechos
-              reservados
-            </p>
-          </div>
-        </div>
-        <div
-          className="site-map"
-          style={{
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <div
-            className="secciones"
-            style={{
-              margin: "0 2.4rem",
-            }}
-          >
-            <h4
-              style={{
-                color: "#FFFFFF",
-              }}
-            >
-              <Link href={"#services"}>Servicios</Link>
+        <div className="flex flex-col lg:flex-row text-center lg:text-left">
+          <div className="my-2 lg:my-0 lg:mx-6">
+            <h4 className="text-white">
+              <Link href="#services">Servicios</Link>
             </h4>
-            <h4
-              style={{
-                color: "#FFFFFF",
-              }}
-            >
-              <Link href={"#projects"}>Proyectos</Link>
+            <h4 className="text-white">
+              <Link href="#projects">Proyectos</Link>
             </h4>
-            <h4
-              style={{
-                color: "#FFFFFF",
-              }}
-            >
-              <Link href={"#about"}>Nosotros</Link>
+            <h4 className="text-white">
+              <Link href="#about">Nosotros</Link>
             </h4>
-            <h4
-              style={{
-                color: "#FFFFFF",
-              }}
-            >
-              <Link href={"#contact"}>Contacto</Link>
+            <h4 className="text-white">
+              <Link href="#contact">Contacto</Link>
             </h4>
           </div>
-          <div
-            className="redes"
-            style={{
-              margin: "0 2.4rem",
-            }}
-          >
-            <h4
-              style={{
-                color: "#FFFFFF",
-              }}
-            >
-              <Link href={"#about"}>Instagram</Link>
+
+          <div className="my-2 lg:my-0 lg:mx-6">
+            <h4 className="text-white">
+              <Link href="#about">Instagram</Link>
             </h4>
-            <h4
-              style={{
-                color: "#FFFFFF",
-              }}
-            >
-              <Link href={"#about"}>LinkedIn</Link>
+            <h4 className="text-white">
+              <Link href="#about">LinkedIn</Link>
             </h4>
-            <h4
-              style={{
-                color: "#FFFFFF",
-              }}
-            >
-              <Link href={"#about"}>WhatsApp</Link>
+            <h4 className="text-white">
+              <Link href="#about">WhatsApp</Link>
             </h4>
           </div>
         </div>
